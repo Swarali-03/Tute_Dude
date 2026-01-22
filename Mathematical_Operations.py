91af777c3920346da9c7cfbe6841ee30b8b4b5b1
Swarali-03 <swaralideolankar1@gmail.com>
2026-01-22 15:55:48 +0530

    Mathematical_Operations.py

---
+++ Mathematical_Operations.py
@@ -0,0 +1,13 @@
+print("Hello Everyone, Let's perform some mathematical operations")
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+
+Addition= a+b
+Subtraction= a-b
+Multiplication= a*b
+Division= a/b
+
+print("Addition=",a+b,
+"\nSubtraction=",a-b,
+"\nMultiplication=",a*b,
+"\nDivision=",a/b)
