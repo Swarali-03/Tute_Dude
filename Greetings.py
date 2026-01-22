c9f0937aabcb77872b4e23e847cbc7ad63fa5239
Swarali-03 <swaralideolankar1@gmail.com>
2026-01-22 16:22:21 +0530

    Greetings

---
+++ Greetings.py
@@ -0,0 +1,6 @@
+print("Hello Everyone, Let's make a greeting")
+
+a=(input("Enter your first name: "))
+b=(input("Enter your last name: "))
+
+print(f"Hello, {a} {b}! Welcome to the Python Program.")
